483335586da3ee58fcbf325a1fe92b393e16dbe6
Maida <maivalenciaolarte@gmail.com>
2024-05-31 00:19:17 -0300

    ....

--- src/Components/CartWidget.jsx
+++ src/Components/CartWidget.jsx
@@ -1,14 +1,18 @@
 import { ShoppingCart } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const CartWidget = ({total, link}) => {
   return (
     <>
       <div className="relative flex items-center">
         <span id='totalItems' className="bg-red-500 z-10 text-white text-xs px-2 py-1 rounded-full absolute bottom-6 left-6">{total}</span>
-        <a href={link} className="px-3">
-          <ShoppingCart width={25} className="hover:text-slate-500 duration-200"/>
+        <Link  to={`/carrito`}>
+        <ShoppingCart width={25} className="hover:text-slate-500 duration-200"/>
           
-        </a>
+        </Link>
+
+
+    
       </div>
     </>
   );
